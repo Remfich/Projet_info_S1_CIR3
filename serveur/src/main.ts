573ab416603714694fd32260974c933f0c57459b
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-01-31 11:27:18 +0100

    Fix client

--- serveur/src/main.ts
+++ serveur/src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Controller, Post, Body } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { Get } from '@nestjs/common/decorators';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,17 +9,3 @@ async function bootstrap() {
   
 }
 bootstrap();
-
-@Controller('api/data')
-export class DataController {
-  @Post()
-  receiveData(@Body() data: any) {
-    console.log(data);
-    return { message: 'Données reçues' };
-  }
-  @Get()
-  sendData() {
-    // Remplacez ceci par les données que vous voulez envoyer
-    return { message: 'Hello, world!' };
-  }
-}
