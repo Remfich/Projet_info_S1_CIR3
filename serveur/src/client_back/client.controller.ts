48e3466984016c214103b6f346c519c6d73c3bd1
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-01-31 16:36:32 +0100

    Init client fonctionnel

--- serveur/src/client_back/client.controller.ts
+++ serveur/src/client_back/client.controller.ts
@@ -1,18 +1,21 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { Produit } from "src/mongoDB/produitDB/produitDB.schema";
 import{requete} from "../utilitaire"
-import { ProduitDto } from "src/mongoDB/produitDB/dto/Produit-dto";
+
+
 
 @Controller('client_back')
 export class ClientBackController {
 
   // Requête pour récupérer les articles de la base de données à l'initialisation de la page
   @Post('/init')
-  async init_client() : Promise<Produit[]>{
+  async init_client() : Promise<Object[]>{
     // On doit demander à la DB la liste des produits pour pouvoir la renvoyer à l'utilisateur
-    const reponse = requete('http://10.224.2.87:3000/produit/getAllProduit',{});
-    console.log(typeof(reponse));
-    
+    const reponse = await requete('http://10.224.2.87:3000/produit/getAllProduit',{});
+    for (let i=0;i<reponse.length;i++){
+      delete reponse[i]._id;
+      delete reponse[i].__v;
+    }
     return reponse;
   }
 }
