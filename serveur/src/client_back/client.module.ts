8901fb56e4428fb45f7bde0f4ea3c5115f837ddf
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-02-01 11:41:07 +0100

    Rangement

--- serveur/src/client_back/client.module.ts
+++ serveur/src/client_back/client.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ClientBackController } from "./client.controller";
-import { ClientBackService } from "./client.service";
 
 @Module({
     imports:[],
     controllers:[ClientBackController],
-    providers:[ClientBackService]
 })
 export class ClientBackModule{}
