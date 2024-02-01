e6df03c67fabc35f38f557b6b4628ff4a0ce00fa
Esteban DESPLANQUES <esteban.desplanques@student.junia.com>
2024-02-01 20:42:30 +0100

    QRScan

--- CodesQr/scan.js
+++ CodesQr/scan.js
@@ -1,5 +1,21 @@
-import "../utilitaire";
-import { ip_serveur, requete } from "../utilitaire";
+const ip_serveur = "http://10.224.2.87";
+
+async function requete(url,donnees) {
+    try {
+      const data = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(donnees)
+      }
+      const reponse = await fetch(url, data);
+      const resultat = await reponse.json();
+      return resultat;
+    } catch (erreur) {
+      console.error("Erreur :", erreur);
+    }
+  }
 function onScanSuccess() {
     var message = document.querySelector("#message");
     message.innerHTML= "Article scanné";
