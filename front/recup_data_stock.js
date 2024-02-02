0d8cd394ef4678f5965ea17b36eaced9971d9aa3
AugusteBuriez <auguste.buriez@student.junia.com>
2024-02-02 15:25:02 +0100

    tkt

--- front/recup_data_stock.js
+++ front/recup_data_stock.js
@@ -46,9 +46,10 @@ async function requete(url,donnees) {
     }
   }
   
-function logout(){
+async function logout(){
     document.cookie = "user=; max-duration = 0; path=/;";
     document.cookie = "admin=; max-duration = 0; path=/;";
+    await new Promise(r => setTimeout(r, 1000));
     document.location.replace(ip_front +":3001/loginAdmin.html")
 }
 
