62a4c0625f9e8102e091f57e71c38b186098fd8c
julien-reynaud <72158202+julien-reynaud@users.noreply.github.com>
2024-02-02 11:10:41 +0100

    graphe qui marche

--- front/chart_data.js
+++ front/chart_data.js
@@ -1,23 +1,49 @@
-const ctx = document.getElementById('myChart');
+statsProduits();
+async function statsProduits(){//Pour récupérer tous les clients de la BDD
+    console.log("balala");
+    var rep = await fetch('http://127.0.0.1:3000/api/data/statsVentes', {
+    method: 'Post',
+    mode:'cors',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(),
+    })
+    if(rep.ok){
+        var stats = await rep.json();
+        stats = stats.data;
+        var noms = []; // Tableau pour stocker les noms de propriété
+        var valeurs = []; // Tableau pour stocker les valeurs
 
-var tabVentes = [12, 19, 3, 5, 2, 3];
-var tabNomProduits = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+        for (var index in stats) {
+            if (stats.hasOwnProperty(index)) {
+                noms.push(index);
+                valeurs.push(stats[index]);
+            }
+        }
+        //Graphe
+        const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-type: 'bar',
-data: {
-    labels: tabNomProduits,
-    datasets: [{
-    label: '# of Votes',
-    data: tabVentes,
-    borderWidth: 1
-    }]
-},
-options: {
-    scales: {
-    y: {
-        beginAtZero: true
-    }
+        var tabVentes = valeurs;
+        var tabNomProduits = noms;
+
+        new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: tabNomProduits,
+            datasets: [{
+            label: 'Nombre de ventes par produit',
+            data: tabVentes,
+            borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+        });
     }
 }
-});
